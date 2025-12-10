f09643becbbf6cff716149357d1f1324862ce684
Ryan Hoffman <ryanhoffman2445@gmail.com>
2025-12-09 23:53:41 -0800

    day 4 part 2

--- day_4.py
+++ day_4.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 with open("day_4_input.txt", "r") as f:
     rolls = [list(line.strip()) for line in f.readlines()]
 
@@ -9,19 +11,49 @@ n = len(rolls[0])
 
 ans = 0
 
+# Part 1
+# for i in range(m):
+#     for j in range(n):
+#         if rolls[i][j] == ".":
+#             continue
+#         found = 0
+#         for change_x, change_y in [*adjacent, *diagonal]:
+#             new_x = i + change_x
+#             new_y = j + change_y
+#             if 0 <= new_x < m and 0 <= new_y < n and rolls[new_x][new_y] == "@":
+#                 found += 1
+#         if found < 4:
+#             ans += 1
+
+# Part 2
+neighbors = [[0] * n for _ in range(m)]
+visited = [[False] * n for _ in range(m)]
+queue = deque([])
+
 for i in range(m):
     for j in range(n):
         if rolls[i][j] == ".":
             continue
-
-        found = 0
         for change_x, change_y in [*adjacent, *diagonal]:
             new_x = i + change_x
             new_y = j + change_y
             if 0 <= new_x < m and 0 <= new_y < n and rolls[new_x][new_y] == "@":
-                found += 1
+                neighbors[i][j] += 1
+        if neighbors[i][j] < 4:
+            queue.append((i, j))
 
-        if found < 4:
-            ans += 1
+while queue:
+    x, y = queue.popleft()
+    if visited[x][y]:
+        continue
+    visited[x][y] = True
+    ans += 1
+    for change_x, change_y in [*adjacent, *diagonal]:
+        new_x = x + change_x
+        new_y = y + change_y
+        if 0 <= new_x < m and 0 <= new_y < n and rolls[new_x][new_y] == "@":
+            neighbors[new_x][new_y] -= 1
+            if neighbors[new_x][new_y] < 4 and not visited[new_x][new_y]:
+                queue.append((new_x, new_y))
 
 print(ans)
