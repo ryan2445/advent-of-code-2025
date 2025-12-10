c34e1259a819965e5eaee05ed6a9c869e495fd56
Ryan Hoffman <ryanhoffman2445@gmail.com>
2025-12-09 22:04:59 -0800

    day 1 part 2

--- day_1.py
+++ day_1.py
@@ -3,21 +3,26 @@ with open('day_1_input.txt', 'r') as f:
     rotations = [line.strip() for line in f.readlines()]
 
 ans = 0
-start = 50
+curr_pos = 50
 
 for rotation in rotations:
   direction = rotation[0]
   distance = int(rotation[1:])
 
-  if direction == 'R':
-    start += distance
-    start %= 100
-  elif direction == 'L':
-    start -= distance
-    start += 100
-    start %= 100
+  full_rotations = distance // 100
+  ans += full_rotations
+  distance %= 100
 
-  if start == 0:
-    ans += 1
+  if direction == 'R':
+    if curr_pos + distance >= 100:
+      ans += 1
+    curr_pos += distance
+    curr_pos %= 100
+  elif direction == 'L':
+    if curr_pos != 0 and curr_pos - distance <= 0:
+      ans += 1
+    curr_pos -= distance
+    curr_pos += 100
+    curr_pos %= 100
 
 print(ans)
